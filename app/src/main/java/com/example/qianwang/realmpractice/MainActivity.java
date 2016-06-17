f87af888af494f6229e3def757051515f7eac8f7
britlovefan <britlovefan@hotmail.com>
2016-06-17 10:29:38 -0700

    exif try
    try to get the exif out of pictures but still don't know that transition yet#

--- app/src/main/java/com/example/qianwang/realmpractice/MainActivity.java
+++ app/src/main/java/com/example/qianwang/realmpractice/MainActivity.java
@@ -4,6 +4,9 @@ import android.media.ExifInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,9 +14,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExifInterface exif = new ExifInterface(pathToImage);
+
+        // get the exif data from the pictures in a folder
+        String pathToImage;
+        File[] files = new File("${rootDir}/../pictures").listFiles();
+        for (File file : files) {
+            if (!file.isFile()) continue;
+            String[] bits = file.getName().split(".");
+            if (bits.length > 0 && bits[bits.length - 1].equalsIgnoreCase("jpg")) {
+                try {
+                    ExifInterface exif = new ExifInterface(file.getAbsolutePath());
+                    String timeDate = exif.getAttribute(ExifInterface.TAG_DATETIME);
+                    String gpsLongitude = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+                    String gpsAltitude = exif.getAttribute(ExifInterface.TAG_GPS_ALTITUDE);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
+
+
 
 
 }
