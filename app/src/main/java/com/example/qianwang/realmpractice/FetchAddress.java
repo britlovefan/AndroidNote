0146731642849d451e589bab8b197416dbad1d72
britlovefan <britlovefan@hotmail.com>
2016-06-17 15:53:08 -0700

    Google for resultreceiver

--- app/src/main/java/com/example/qianwang/realmpractice/FetchAddress.java
+++ app/src/main/java/com/example/qianwang/realmpractice/FetchAddress.java
@@ -9,10 +9,12 @@ import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -47,10 +49,27 @@ public class FetchAddress extends IntentService {
                     ", Longitude = " +
                     location.getLongitude(), illegalArgumentException);
         }
-
-
-    }
-
+        //if no address is found
+        if(possibleAddress==null||possibleAddress.size()==0){
+            if (errorMessage.isEmpty()) {
+                errorMessage = getString(R.string.no_address_found);
+                Log.e(TAG, errorMessage);
+            }
+            deliverResult(Constants.FAILURE_RESULT, errorMessage);
+        } else {
+            Address address = possibleAddress.get(0);
+            ArrayList<String> addressFragments = new ArrayList<String>();
+            for(int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                addressFragments.add(address.getAddressLine(i));
+            }
+            Log.i(TAG, getString(R.string.address_found));
+            deliverResult(Constants.SUCCESS_RESULT,
+                    TextUtils.join(System.getProperty("line.separator"),
+                            addressFragments));
+        }
+        }
+    //deliver the result code revealing whether the address have been successfully retrieved
+    //Along with the address itself
     private void deliverResult(int resultCode,String message){
         Bundle bundle = new Bundle();
         bundle.putString(Constants.RESULT_DATA_KEY, message);
--- app/src/main/java/com/example/qianwang/realmpractice/MainActivity.java
+++ app/src/main/java/com/example/qianwang/realmpractice/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.qianwang.realmpractice;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.media.ExifInterface;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.CharacterStyle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,14 +19,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AddressResultReceiver mResultReceiver;
+    int resultcode;
+    Bundle bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
 
         // get the exif data from the pictures in a folder
         // the directory of the photos should be modified to be the folder of photos in android phone
@@ -28,28 +34,37 @@ public class MainActivity extends AppCompatActivity {
             if (!file.isFile()) continue;
             String[] bits = file.getName().split(".");
             if (bits.length > 0 && bits[bits.length - 1].equalsIgnoreCase("jpg")) {
-                    String imagePath = file.getAbsolutePath();
-                    Location curLocation = readGeoTagImage(imagePath);
+                String imagePath = file.getAbsolutePath();
+                Location curLocation = readGeoTagImage(imagePath);
+                startIntentService(curLocation);
+                mResultReceiver.onReceiveResult(resultcode,bundle);
 
             }
         }
-
     }
+
+    // start intent service for each gps data pair
+    protected void startIntentService(Location location) {
+        Intent intent = new Intent(this, FetchAddress.class);
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, location);
+        startService(intent);
+    }
+
+
     // the function that returns the location object
-    public Location readGeoTagImage(String imagePath)
-    {
+    public Location readGeoTagImage(String imagePath) {
         Location loc = new Location("");
         try {
             ExifInterface exif = new ExifInterface(imagePath);
-            float [] latlong = new float[2] ;
-            if(exif.getLatLong(latlong)){
+            float[] latlong = new float[2];
+            if (exif.getLatLong(latlong)) {
                 loc.setLatitude(latlong[0]);
                 loc.setLongitude(latlong[1]);
             }
             String date = exif.getAttribute(ExifInterface.TAG_DATETIME);
             SimpleDateFormat fmt_Exif = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
             loc.setTime(fmt_Exif.parse(date).getTime());
-
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
@@ -58,7 +73,25 @@ public class MainActivity extends AppCompatActivity {
         return loc;
     }
 
+    //
+    @SuppressLint("ParcelCreator")
+    class AddressResultReceiver extends ResultReceiver {
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
 
-
-
+            // Display the address string
+            // or an error message sent from the intent service.
+            String mAddressOutput = resultData.getString(Constants.RESULT_DATA_KEY);
+            // Show a toast message if an address was found.
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.address_found);
+                Toast toast =Toast.makeText(context,text, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    }
 }
