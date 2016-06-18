97ec63bfd5bb72eefa3b1576efcb823e93ac2ae6
britlovefan <britlovefan@hotmail.com>
2016-06-17 17:22:38 -0700

    Start to implement models

--- app/src/main/java/com/example/qianwang/realmpractice/Photo.java
+++ app/src/main/java/com/example/qianwang/realmpractice/Photo.java
@@ -5,8 +5,9 @@ import io.realm.RealmObject;
  * Created by qianwang on 6/17/16.
  */
 public class Photo extends RealmObject {
-
-
-
-
+    private String name;
+    private String ZipCode;
+    private String state;
+    private String city;
+    private String place;
 }
