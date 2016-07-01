985d609e6fe4eeefce4370ae808c12f42eb01623
britlovefan <britlovefan@hotmail.com>
2016-07-01 16:19:20 -0700

    Trying to show the customize icon of the cluster in the last level, still in process

--- app/src/main/java/com/example/qianwang/realmpractice/OptionChooser.java
+++ app/src/main/java/com/example/qianwang/realmpractice/OptionChooser.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.qianwang.realmpractice.map.MapCluster;
+
 /**
  * Created by qianwang on 6/30/16.
  */
